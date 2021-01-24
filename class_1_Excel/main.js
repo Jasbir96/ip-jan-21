b92a3888348140b6e955eb1020e092157454607e
Jasbir <mr.jasbirsingh96@gmail.com>
2021-01-24 21:05:42 +0530

    excel lecture-1

---
+++ class_1_Excel/main.js
@@ -0,0 +1,32 @@
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const ejs = require("ejs-electron");
+// c++ binding
+ejs.data({
+    title: "My Sheet 1"
+})
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+    })
+    win.loadFile('index.ejs').then(() => {
+        win.webContents.openDevTools();
+        win.maximize();
+    })
+}
+app.whenReady().then(createWindow);
+// mac 
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
